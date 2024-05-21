aeea8bb5fe83c2185a25bd9b49733491b8a2f280
Assaf <1214assaf@gmail.com>
2024-05-21 16:01:18 +0300

    refactor: Update cartSlice.js to handle adding and removing items in the cart

---
+++ src/store/cartSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItem(state, action) {
+      const item = action.payload;
+      const existingItem = state.items.find((item) => item.id === item.id);
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.totalPrice += item.price;
+      } else {
+        state.items.push({
+          id: item.id,
+          price: item.price,
+          quantity: 1,
+          totalPrice: item.price,
+          name: item.title,
+        });
+      }
+    },
+    removeItem(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice.reducer;
