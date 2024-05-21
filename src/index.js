655e8f9322cebb056eabceafb20a3bfded8d0d12
Assaf <1214assaf@gmail.com>
2024-05-21 15:54:08 +0300

    refactor: Update ReactDOM import and use createRoot for rendering
    This commit updates the import statement for ReactDOM in the index.js file to use the new createRoot function from the react-dom/client module. It also wraps the App component with the Provider component from react-redux to provide the Redux store to the entire application.
    
    The changes improve the performance of the application by using the createRoot function for rendering and enabling the use of Redux for state management.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import store from "./store";
 
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
