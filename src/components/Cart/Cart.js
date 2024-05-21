53a92c46b4a9ad6d07206e0b201acc92646cd163
Assaf <1214assaf@gmail.com>
2024-05-21 16:49:02 +0300

    refactor: Update Cart, CartButton, CartItem, ProductItem, and Products components
    This commit updates multiple components in the codebase. The Cart component now uses the useSelector hook from react-redux to retrieve items from the cart state. The CartItem component has been modified to dispatch actions for removing and adding items to the cart. The CartButton component now displays the total quantity of items in the cart by using the useSelector hook. The ProductItem component dispatches an action to add items to the cart. Lastly, the Products component now dynamically renders a list of products using the DUMMY_PRODUCTS data.
    
    These changes improve the functionality and user experience of the shopping cart feature.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,28 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import Card from "../UI/Card";
+import { useSelector } from "react-redux";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const items = useSelector((state) => state.cart.items);
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {items.map((item) => {
+          return (
+            <CartItem
+              id={item.id}
+              key={item.id}
+              item={{
+                name: item.title,
+                quantity: item.quantity,
+                total: item.totalPrice,
+                price: item.price,
+              }}
+            />
+          );
+        })}
       </ul>
     </Card>
   );
